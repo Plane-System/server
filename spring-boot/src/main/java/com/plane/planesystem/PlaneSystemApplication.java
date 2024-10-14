1a4f45a2a08faacc78f3d7d381bbe8e85e839ced
DinhB <dinhb@splus-software.com.vn>
2024-10-14 20:58:27 +0700

    init: first code

---
+++ spring-boot/src/main/java/com/plane/planesystem/PlaneSystemApplication.java
@@ -0,0 +1,13 @@
+package com.plane.planesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneSystemApplication.class, args);
+	}
+
+}
---
+++ spring-boot/src/test/java/com/plane/planesystem/PlaneSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plane.planesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
